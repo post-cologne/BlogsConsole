862190c4b51ff458a4fef71458f12e899cd597e3
Post-Cologne <vr2025.pr@outlook.com>
2025-10-28 13:31:07 -0500

    [1.06-1.09]finish class Demo

--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+
+// Create and save a new Blog
+Console.Write("Enter a name for a new Blog: ");
+var name = Console.ReadLine();
+
+var blog = new Blog { Name = name };
+
+var db = new DataContext();
+db.AddBlog(blog);
+logger.Info("Blog added - {name}", name);
+
+// Display all Blogs from the database
+var query = db.Blogs.OrderBy(b => b.Name);
+
+Console.WriteLine("All blogs in the database:");
+foreach (var item in query)
+{
+  Console.WriteLine(item.Name);
+}
 
 logger.Info("Program ended");
